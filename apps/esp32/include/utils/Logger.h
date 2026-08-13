52aca96be5ba0a748ad848a7940b1972eae11cf7
Dennis <dennishoyer@proton.me>
2026-08-13 21:33:31 +0000

    debug(esp32): Lib-Debug-Logs in Ringbuffer spiegeln (RingStream), 1.6.3

--- apps/esp32/include/utils/Logger.h
+++ apps/esp32/include/utils/Logger.h
@@ -18,9 +18,30 @@ void logPrint(const char* type, const char* format, ...) __attribute__((format(p
 // Function to log messages to Serial and WebSocket with default type "log"
 void logPrint(const char* format, ...);
 
-// Ringbuffer: speichert die letzten Log-Zeilen für den Debug-Server.
-// Thread-sicher via Interrupt-Sperre (Logger wird aus mehreren Tasks aufgerufen).
 void logRingPush(const char* type, const char* line);
+
+// Stream der Serial-Ausgaben abfängt und in den Ringbuffer spiegelt (für Lib-Debug-Logs)
+class RingStream : public Print {
+public:
+    size_t write(uint8_t c) override {
+        Serial.write(c);
+        if (c == '\n') {
+            if (bufLen > 0) {
+                buf[bufLen] = '\0';
+                logRingPush("lib", buf);
+                bufLen = 0;
+            }
+        } else if (bufLen < LOG_LINE_MAX - 1) {
+            buf[bufLen++] = c;
+        }
+        return 1;
+    }
+private:
+    char buf[LOG_LINE_MAX];
+    size_t bufLen = 0;
+};
+
+// Ringbuffer: speichert die letzten Log-Zeilen für den Debug-Server.
 // Gibt die Zeilen als JSON-Array zurück: {"since":<ts>,"lines":[{"ts":<ms>,"t":"type","m":"msg"}]}
 // since = Millis-Timestamp des zuletzt gelieferten Logs (0 = alle).
 String logRingJson(uint32_t since);
